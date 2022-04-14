d2fc81bae94ec81c323b17b40d9d25cde34bc896
Luis Masuelli <luis@datagran.io>
2022-04-14 12:00:15 -0500

    Moving everything into a single file.

--- http_storage/flask_app.py
+++ http_storage/flask_app.py
@@ -1,9 +1,19 @@
+import functools
+import os
 import typing as t
 from datetime import date, datetime
+from urllib.parse import quote_plus
 from bson import ObjectId
 from cerberus import Validator, TypeDefinition
-from flask import Flask, current_app as app
-from flask.json import JSONEncoder
+from pymongo import MongoClient
+from flask import Flask, current_app as app, make_response, request
+from flask.json import JSONEncoder, jsonify
+
+
+class ImproperlyConfiguredError(Exception):
+    """
+    Raised when the storage app is misconfigured.
+    """
 
 
 _DATETIME_FORMATS = [
@@ -13,6 +23,13 @@ _DATETIME_FORMATS = [
     "%Y-%m-%dT%H:%M:%S",
 ]
 _DATE_FORMAT = "%Y-%m-%d"
+
+
+USER = os.environ['MONGODB_USER']
+PASSWORD = os.environ['MONGODB_PASSWORD']
+HOST = os.environ.get('MONGODB_HOST', 'localhost')
+PORT = os.environ.get('MONGODB_PORT', '27017')
+client = MongoClient("mongodb://%s:%s@%s:%s" % (quote_plus(USER), quote_plus(PASSWORD), HOST, PORT))
 
 
 class MongoDBEnhancedEncoder(JSONEncoder):
@@ -114,3 +131,90 @@ class StorageApp(Flask):
     timestamp_with_splitseconds = False
     auth_db = None
     auth_table = None
+
+    def __init__(self, auth_db=None, auth_table=None, validator_class=None, *args, **kwargs):
+        """
+        Checks a validator_class is properly configured, as well as the auth_db / auth_table.
+
+        :param auth_db: The db, in mongo server, used for authentication.
+        :param auth_table: The table, in mongo server, used for authentication.
+        :param args: The flask-expected positional arguments.
+        :param kwargs: The flask-expected keyword arguments.
+        """
+
+        # First, set the non-None arguments, overriding the per-class setup.
+        self._validator_class = validator_class or self.validator_class
+        self._auth_db = auth_db or self.auth_db
+        self._auth_table = auth_table or self.auth_table
+
+        # Also, set everything up to keep schema decorators.
+        self._schema_decorators = {}
+
+        # Then, validate them (regardless being instance or class arguments).
+        if not (isinstance(self._validator_class, type) and issubclass(self._validator_class, MongoDBEnhancedValidator)):
+            raise ImproperlyConfiguredError("Wrong or missing validator class")
+        if not (self._auth_db and self._auth_table):
+            raise ImproperlyConfiguredError("Wrong or missing auth_db / auth_table settings")
+
+        super().__init__(*args, **kwargs)
+
+    def _bearer_required(self, f):
+        """
+        Requires a valid "Authorization: Bearer xxxxx..." header.
+        :param f: The function to invoke.
+        :return: The decorated function.
+        """
+
+        def wrapper(*args, **kwargs):
+            # Get the auth settings.
+            auth_db = self._auth_db
+            auth_table = self._auth_table
+            # Get the header. It must be "bearer {token}".
+            authorization = request.headers.get('Authorization')
+            if not authorization:
+                return make_response(jsonify({'code': 'authorization:missing-header'}), 401)
+            # Split it, and expect it to be "bearer".
+            try:
+                scheme, token = authorization.split(' ')
+                if scheme.lower() != 'bearer':
+                    return make_response(jsonify({'code': 'authorization:bad-scheme'}), 400)
+            except ValueError:
+                return make_response(jsonify({'code': 'authorization:syntax-error'}), 400)
+            # Check the token.
+            token = client[auth_db][auth_table].find_one({'_id': ObjectId(token),
+                                                          'valid_until': {"$not": {"$lt": datetime.now()}}})
+            if not token:
+                return make_response(jsonify({'code': 'authorization:not-found'}), 401)
+            # If the validation passed, then we invoke the decorated function.
+            return f(*args, **kwargs)
+        return wrapper
+
+    def _schema_required(self, schema):
+        """
+        Returns a decorator, with a given schema, that wraps a function
+        that requires a json body matching the given schema in order to
+        proceed.
+        :return: A decorator.
+        """
+
+        MongoDBEnhancedValidator.apply_default_coercers(schema)
+        # Get the id of the schema.
+        schema_id = id(schema)
+        # Attempt to get the already-existing schema decorator.
+        schema_decorator = self._schema_decorators.get(schema_id)
+        if schema_decorator:
+            return schema_decorator
+
+        # Define and store the decorator, and return it.
+        def wrapper(f):
+            validator = self._validator_class(schema)
+
+            # This is the function that will run the logic.
+            @functools.wraps(f)
+            def wrapped(*args, **kwargs):
+                # TODO implement all the checks.
+                return f(*args, **kwargs)
+            return wrapped
+
+        self._schema_decorators[schema_id] = wrapper
+        return wrapper

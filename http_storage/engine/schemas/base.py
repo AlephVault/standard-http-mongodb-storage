c3d409cddf566c6c6e9a177ed760fd7483e787cd
Luis Masuelli <luis@datagran.io>
2022-04-20 16:36:39 -0500

    Adding more details to the resources.

--- http_storage/engine/schemas/base.py
+++ http_storage/engine/schemas/base.py
@@ -7,7 +7,17 @@ RESOURCE = {
         "type": "string",
         "required": True,
         "allowed": ["list", "simple"]
-    }
+    },
+    "db": {
+        "type": "string",
+        "required": True,
+        "regex": "[a-zA-Z][a-zA-Z0-9_-]+"
+    },
+    "collection": {
+        "type": "string",
+        "required": True,
+        "regex": "[a-zA-Z][a-zA-Z0-9_-]+"
+    },
 }
 schema_registry.add("http_storage.schemas.resource", RESOURCE)
 
@@ -33,6 +43,22 @@ SETTINGS = {
                 "type": "string",
                 "default_setter": lambda doc: os.getenv('MONGODB_PASSWORD', '')
             }
+        }
+    },
+    "auth": {
+        "type": "dict",
+        "default_setter": lambda doc: {},
+        "schema": {
+            "db": {
+                "type": "string",
+                "regex": "[a-zA-Z][a-zA-Z0-9_-]+",
+                "default_setter": lambda doc: os.getenv("APP_AUTH_DB", "http_storage")
+            },
+            "collection": {
+                "type": "string",
+                "regex": "[a-zA-Z][a-zA-Z0-9_-]+",
+                "default_setter": lambda doc: os.getenv("APP_AUTH_DB", "auth")
+            },
         }
     },
     "resources": {

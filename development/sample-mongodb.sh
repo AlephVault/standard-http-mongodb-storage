c0a1f8ea5e03844575feaaea59c8722dcf12e415
Luis Masuelli <luis@datagran.io>
2022-04-25 22:59:56 -0500

    Adding a .gitignore setting and a better example for docker.

--- development/sample-mongodb.sh
+++ development/sample-mongodb.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run --name mongodb-dev --rm -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=p455w0rd mongo:latest
+BASEDIR=$(basedir "$(dirname $0)")
+docker run --name mongodb-dev --rm -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=p455w0rd -v $BASEDIR/.tmp/mongo:/data/db mongo:latest
